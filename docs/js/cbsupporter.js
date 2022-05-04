304d73d7e586e8736575eb58f551830eaa09bddb
roppy001 <roppy.dreamvision200@gmail.com>
2022-05-05 03:56:16 +0900

    クリップボードへのコピー＆ペースト機能を実装

--- docs/js/cbsupporter.js
+++ docs/js/cbsupporter.js
@@ -1,30 +1,36 @@
 
-function displayResultTl( preTl, takeoverTime){
-
+function calculateResultTl(){
+    var preText = $('#pre_tl').val();
+    $('#post_tl').val(preText);
 }
 
+
 function pasteInputTl(){
-    
     if(navigator.clipboard){
         navigator.clipboard.readText()
-        .then(function(text){
+        .then(function (text) {
             $('#pre_tl').val(text);
+            calculateResultTl();
         });
     }
 }
 
+function copyResultTl(){
+    navigator.clipboard.writeText($('#post_tl').val()).then(function (){alert('結果をクリップボードにコピーしました');});
+}
+
+
 function initEventHandler(){
     $('#paste_input_tl').on('click', pasteInputTl);
-    $('#copy_result_tl1').on('click',function(){
-    });
-    $('#copy_result_tl2').on('click',function(){
-    });
+    $('#copy_result_tl1').on('click',copyResultTl);
+    $('#copy_result_tl2').on('click',copyResultTl);
     $('#takeover_time').on('change',function(){
         // 整数に再セット
 
-
+        calculateResultTl();
     });
     $('#pre_tl').on('change',function(){
+        calculateResultTl();
     });
 }  
 
