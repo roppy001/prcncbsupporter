3a4c07de93ec9ccdbab155af1a2b890052b561c0
roppy001 <roppy.dreamvision200@gmail.com>
2022-05-05 05:21:05 +0900

    時刻変換機能を実装

--- docs/js/cbsupporter.js
+++ docs/js/cbsupporter.js
@@ -1,7 +1,40 @@
 
+function decodeTime(tstr){
+    var arr = tstr.split(':');
+    return parseInt(arr[0])*60+parseInt(arr[1]);
+}
+
+function encodeTime(t){
+    s = t % 60;
+    return parseInt(t/60) + ':' + ((s < 10) ? '0' : '') + s;
+}
+
 function calculateResultTl(){
     var preText = $('#pre_tl').val();
-    $('#post_tl').val(preText);
+    var takeoverTime = parseInt($('#takeover_time').val());
+
+    var postText = '＜持越'+takeoverTime+'秒のTL＞\n';
+    var i;
+    var linearray = preText.split(/\r\n|\n/);
+    var flg=true;
+    var zeroCutFlg = $('#zero_cut').prop('checked');
+
+    for(i=0;i<linearray.length;i++){
+        var tmpstr = linearray[i].replaceAll(/\d{1,2}:\d{2}/g,
+        function(m, offset){
+            var t = Math.max(0, decodeTime(m) + takeoverTime - 90);
+            if (t == 0 && offset == 0) {
+                flg=false;
+            }
+            return encodeTime(t);
+        } );
+
+        if(flg || !zeroCutFlg){
+            postText += tmpstr + '\n';
+        }
+    }
+
+    $('#post_tl').val(postText);
 }
 
 
@@ -25,11 +58,17 @@ function initEventHandler(){
     $('#copy_result_tl1').on('click',copyResultTl);
     $('#copy_result_tl2').on('click',copyResultTl);
     $('#takeover_time').on('change',function(){
-        // 整数に再セット
+        // 整数に再セット、21～90の値に強制変換
+        var n = parseInt($('#takeover_time').val());
+        n = Math.min(90,Math.max(21, n));
+        $('#takeover_time').val(n);
 
         calculateResultTl();
     });
     $('#pre_tl').on('change',function(){
+        calculateResultTl();
+    });
+    $('#zero_cut').on('change',function(){
         calculateResultTl();
     });
 }  
